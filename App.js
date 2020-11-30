0aa3705a3b5f3e7e481ca3fae424d05816210760
aishacodes <aishatabdulfatah81@gmail.com>
2020-11-30 13:46:28 +0100

    feat: updated transpage

--- App.js
+++ App.js
@@ -1,25 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import {View, Text, SafeAreaView, StyleSheet, Platform, Image } from 'react-native';
+import {
+  View,
+  Text,
+  TextInput,
+  SafeAreaView,
+  StyleSheet,
+  StatusBar,
+  Platform,
+  Image,
+  TouchableHighlight,
+  TouchableNativeFeedback,
+} from 'react-native';
 
-import profilewhite from './assets/profile.png'
-import logo from './assets/abeg.png'
-import not from './assets/not.png'
+import AbegIcon from './components/icons/AbegIcon';
+import ArrowIcon from './components/icons/Arrow';
+import HeartIcon from './components/icons/Heart';
+import LockIcon from './components/icons/lock';
+import WalletIcon from './components/icons/wallet';
+import GroupIcon from './components/icons/group'
+import Aicon from './components/icons/aicon'
 
+import profilewhite from './assets/profile.png';
+import not from './assets/not.png';
+
+const Touchable =
+  Platform.OS === 'android' ? TouchableNativeFeedback : TouchableHighlight;
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-    <View style={styles.topNav}>
-      <Image style={styles.topimg} source={profilewhite} />
-       <Image source={logo} />
-       <Image style={styles.topimg} source={not} />
-    </View>
-    <View style={styles.price}>
-      <Text style={[styles.textWhite, styles.sym]}> N </Text> 
-      <Text style={styles.textWhite}> 0</Text> 
-    </View>
-    <View style= {styles.reas}><Text>What is it for'</Text></View>
+      <View style={styles.topNav}>
+        <Image style={styles.topimg} source={profilewhite} />
+        <AbegIcon />
+        <Image style={styles.topimg} source={not} />
+      </View>
+      <View style={styles.price}>
+        <Text style={[styles.textWhite, styles.sym]}> ₦ </Text>
+        <Text style={styles.textWhite}> 0</Text>
+      </View>
+      <TextInput placeholder="What is it for?" placeholderTextColor= "white" style={styles.reas} />
+      <View style={styles.numgrid}>
+        <View style={styles.num}>
+          <Text style={styles.numText}>1</Text>
+          <Text style={styles.numText}>2</Text>
+          <Text style={styles.numText}>3</Text>
+        </View>
+        <View style={styles.num}>
+          <Text style={styles.numText}>4</Text>
+          <Text style={styles.numText}>5</Text>
+          <Text style={styles.numText}>6</Text>
+        </View>
+        <View style={styles.num}>
+          <Text style={styles.numText}>7</Text>
+          <Text style={styles.numText}>8</Text>
+          <Text style={styles.numText}>9</Text>
+        </View>
+        <View style={styles.num}>
+          <Text style={styles.numText}>00</Text>
+          <Text style={styles.numText}>0</Text>
+          <ArrowIcon/>
+        </View>
+      </View>
+      <View style={styles.actions}>
+        <Touchable onPress={() => console.log('how mosh')}>
+          <Text style={styles.action}>REQUEST</Text>
+        </Touchable>
+        <Touchable>
+          <Text style={styles.action}>SEND</Text>
+        </Touchable>
+      </View>
+      <View style={styles.bottomNav}>
+        <LockIcon style={{fill: 'rgba(0,0,0,0.3)'}} />
+        <HeartIcon style={{fill: 'rgba(0,0,0,0.3)'}}/>
+        <Aicon style={styles.aiicon} />
+        <GroupIcon style={{fill: 'rgba(0,0,0,0.3)'}}/>
+        <WalletIcon style={{fill: 'rgba(0,0,0,0.3)'}}/>
+      </View>
     </SafeAreaView>
   );
 }
@@ -29,49 +85,89 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#7e3bff',
     paddingTop: Platform.OS === 'android' ? StatusBar.currentHeight : 0,
-    paddingHorizontal: 20,
-    color: 'white'
+    paddingBottom: StatusBar.currentHeight,
+    color: 'white',
   },
   topNav: {
+    paddingHorizontal: 10,
     flexDirection: 'row',
-    alignItems: "center",
-    justifyContent: "space-between",
-    paddingVertical: 30
-    
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingVertical: 15,
   },
   topimg: {
     width: 30,
-    height: 30
+    height: 30,
   },
   price: {
-    flexDirection:"row",
-    justifyContent: "center",
-    alignItems: 'center'
-
+    flexDirection: 'row',
+    justifyContent: 'center',
+    alignItems: 'center',
   },
-   textWhite: {
+  textWhite: {
     color: 'white',
-    fontSize: 30
-
+    fontSize: 30,
   },
   sym: {
-    borderRadius: 5,
+    borderRadius: 20,
     backgroundColor: 'rgba(0,0,0,0.07)',
     padding: 7,
-
-  }, 
+  },
   reas: {
     flexDirection: 'row',
-    justifyContent: "center",
-    paddingVertical: 8,
+    justifyContent: 'center',
     borderWidth: 1,
-    padding: 20,
-    borderRadius: 10,
-    marginVertical: 20,
+    paddingVertical: 5,
+    paddingHorizontal: 10,
+    borderRadius: 30,
+    marginVertical: 30,
+    marginHorizontal: '20%',
+    borderColor: 'white',
+    color: 'white',
+  },
+  numgrid: {
+    // marginHorizontal: 10,
+    flex: 1,
+    justifyContent: 'space-between',
+  },
+  num: {
+    flexDirection: 'row',
+    justifyContent: "space-around"
 
-    
-
+  },
+  numText: {
+    color: 'white',
+    fontSize: 30,
+    fontWeight: '500',
+    textAlign: 'center',
+  },
+  actions: {
+    flexDirection: 'row',
+    paddingRight: 20,
+    paddingLeft: 30,
+    paddingVertical: 10
+  },
+  action: {
+    flex: 1,
+    paddingVertical: 20,
+    paddingHorizontal: 20,
+    marginRight: 10,
+    backgroundColor: 'rgba(0,0,0,0.07)',
+    borderRadius: 20,
+    color: 'white',
+    textAlign: 'center',
+    marginTop: 10
+  },
+  bottomNav: {
+    flexDirection: "row",
+    alignItems: 'center',
+    justifyContent: "space-around",
+    alignContent: "flex-end"
+  }, 
+  aiicon: {
+    borderRadius: 20,
+    backgroundColor: 'rgba(0,0,0,0.07)',
+    padding: 7,
   }
-
-
 });
+
